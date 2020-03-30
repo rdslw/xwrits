32a775d48443563334dcc88feb6d7d83a7b76e63
Rado Sław <rdslw@ferusfragum.science>
2020-03-30 21:44:58 +0200

    Build process supports missing Xinerama and present XSS

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
+grep -qiE '^NAME=.*(ubuntu|debian)' /etc/os-release
+if [ $? -eq 0 ]; then
+    echo "Xwrits build process requires devel packages, will try apt-get them.."
+    sudo apt-get install x11lib-dev autoconf libxss1
+fi
 autoreconf -i
