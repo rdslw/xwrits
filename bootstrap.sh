459817cfe97b41c11c7606c8cd25e3cd33a59538
Rado Sław <rdslw@ferusfragum.science>
2020-03-30 23:01:37 +0200

    Fixed build process on ubuntu (libs), and disable xss there

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 grep -qiE '^NAME=.*(ubuntu|debian)' /etc/os-release
 if [ $? -eq 0 ]; then
     echo "Xwrits build process requires devel packages, will try apt-get them.."
-    sudo apt-get install x11lib-dev autoconf libxss1
+    sudo apt-get install autoconf x11lib-dev libxext-dev
+    # xss is preferred, until electron (e.g. slack ) is used :/ 
+    # sudo apt-get install libxss1 libxss-dev
 fi
 autoreconf -i
